b3738fe93943dc51865d148160898f2cab64357b
Mark Keaton <mkeaton@gmail.com>
2020-07-21 17:07:31 -0500

    Exclude map and tests actually.

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+setup_requires = ["munch>=2.5.0", "pydash>=4.8.0"]
+excluded = ["tests/", "maps/"]
+
 setuptools.setup(
     name="pystyx",
-    version="0.3.1",
+    version="0.3.2",
     scripts=[],
     author="Mark Keaton",
     author_email="mkeaton@gmail.com",
@@ -13,11 +16,10 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/styx-dev/pystyx",
-    packages=setuptools.find_packages(exclude=["tests", "maps"]),
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    setup_requires=["munch>=2.5.0", "pydash>=4.8.0"],
 )
