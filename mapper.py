69cd30547369e5d417038627d9b811c96eb268cf
Mark Keaton <mkeaton@gmail.com>
2020-07-21 09:58:06 -0500

    Extract Mapper into dependent classes.

--- mapper.py
+++ mapper.py
@@ -1,9 +1,8 @@
-from typing import Callable, Dict, List
+from typing import Callable, Dict, Literal
 
 from munch import Munch, munchify
 from pydash import get, set_
 
-from .functions import TomlFunction
 from .parser import Parser
 
 
@@ -11,84 +10,141 @@ def empty_functions_toml():
     return munchify({"functions": []})
 
 
-class Mapper:
+class ProcessMapper:
+    definition: Munch
+    definitions: Dict[str, Munch]
     functions: Dict[str, Callable]
-    map: Munch
-    mapped_object: any
-    maps: Dict[str, Munch]
+    processor_key: Literal["preprocess", "postprocess"] = NotImplementedError
+
+    def __init__(self, definition, functions, definitions):
+        self.definition = definition
+        self.definitions = definitions
+        self.functions = functions
+
+    def __call__(self, obj):
+        """
+        Depending on whether it is preprocess or postprocess,
+        the obj will either be the from_obj or the to_obj.
+
+        That is, preprocess prepares the from_obj for processing.
+        Postprocess polishes the to_obj for final export.
+        """
+        if hasattr(self.definition, self.processor_key):
+            process_dict = getattr(self.definition, self.processor_key)
+            processors = sorted(
+                [(key, value) for key, value in process_dict.items()],
+                key=lambda pair: pair[0],
+            )
+            for (_key, processor) in processors:
+                func = self.functions[processor.transform]
+                old_value = get(
+                    obj,
+                    processor.path,
+                    processor.or_else if processor.or_else else None,
+                )
+                new_value = func(old_value)
+                set_(obj, processor.path, new_value)
+            return obj
+
+
+class PreprocessMapper(ProcessMapper):
+    processor_key = "preprocess"
+
+
+class PostprocessMapper(ProcessMapper):
+    processor_key = "postprocess"
+
+
+class FieldsMapper:
+    definition: Munch
+    definitions: Dict[str, Munch]
+    functions: Dict[str, Callable]
+
+    def __init__(self, definition, functions, definitions):
+        self.definition = definition
+        self.definitions = definitions
+        self.functions = functions
+
+    def __call__(self, from_obj):
+        to_obj = {}  # TODO: Add other structures potentially
+
+        for field_name, field_definition in self.definition.fields.items():
+            to_obj = self.map_field(from_obj, to_obj, field_name, field_definition)
+
+        return to_obj
+
+    def map_field(self, from_obj, to_obj, field_name, field_definition):
+        value = self.get_field_value(from_obj, field_definition)
+        set_(to_obj, field_name, value)
+        return to_obj
+
+    def get_field_value(self, from_obj, field_definition):
+        if field_definition.get("possible_paths"):
+            options = [
+                get(from_obj, path, None)
+                for path in field_definition.input_path_options
+            ]
+            condition = field_definition.options_condition
+            potential_values = list(
+                filter(
+                    lambda val: get(val, condition.field) == condition.value, options,
+                )
+            )
+
+            if len(potential_values) > 1:
+                raise RuntimeError(
+                    "Unable to determine input path. Found more than one option satisfying predicate."
+                )
+
+            if not potential_values:
+                raise RuntimeError(
+                    "Unable to determine input path. Unable to find option satisfying predicate."
+                )
+
+            value = potential_values[0]
+        else:
+            value = get(from_obj, field_definition.input_path)
+
+        # TODO: Pass off Foreign key to other TOML Object (Address) for handling
+        return value
+
+
+class Mapper:
+    definition: Munch
+    definitions: Dict[str, Munch]
+    fieldsMapper: FieldsMapper
+    fieldsMapperClass = FieldsMapper
+    functions: Dict[str, Callable]
+    preprocessMapper: PreprocessMapper
+    preprocessMapperClass = PreprocessMapper
+    postprocessMapper: PostprocessMapper
+    postprocessMapperClass = PostprocessMapper
     raw_map: Munch
     type: str
 
-    def __init__(self, toml_map, functions, maps=None):
+    def __init__(self, toml_map, functions, definitions=None):
         self.raw_map = toml_map
-        self.map = self.parse_map(self.raw_map)
-        self.mapped_object = {}  # TOOD: Allow for other structures
-        self.type = self.map.type
-        self.maps = maps if maps is not None else {}
+        self.definition = self.parse_definition(self.raw_map)
+        self.type = self.definition.type
+        self.definitions = definitions if definitions is not None else {}
         self.functions = functions
 
-    def __call__(self, blob: any):
-        blob = self.preprocess(blob)
-        blob = self.fields(blob)
-        blob = self.postprocess(blob)
-        return blob
+        self.preprocessMapper = self.preprocessMapperClass(
+            self.definition, functions, self.definitions
+        )
+        self.fieldsMapper = self.fieldsMapperClass(
+            self.definition, functions, self.definitions
+        )
+        self.postprocessMapper = self.postprocessMapperClass(
+            self.definition, functions, self.definitions
+        )
 
-    def parse_map(self, toml_map: Munch) -> Munch:
+    def __call__(self, from_obj: any):
+        from_obj = self.preprocessMapper(from_obj)
+        to_obj = self.fieldsMapper(from_obj)
+        to_obj = self.postprocessMapper(to_obj)
+        return to_obj
+
+    def parse_definition(self, toml_map: Munch) -> Munch:
         parser = Parser()
         return parser.parse(toml_map)
-
-    def preprocess(self, blob):
-        if self.map.preprocess:
-            preprocessors = sorted(
-                [(key, value) for key, value in self.map.preprocess.items()],
-                key=lambda pair: pair[0],
-            )
-            for (_key, preprocessor) in preprocessors:
-                func = self.functions[preprocessor.transform]
-                old_value = get(
-                    blob,
-                    preprocessor.path,
-                    preprocessor.or_else if preprocessor.or_else else None,
-                )
-                new_value = func(old_value)
-                set_(blob, preprocessor.path, new_value)
-            return blob
-
-    def fields(self, blob):
-        for field, action in self.map.fields.items():
-            if action.get("input_path") and action.get("input_path_options"):
-                raise RuntimeError(
-                    "input_path and input_path_options cannot both be defined."
-                )
-
-            if action.get("input_path_options"):
-                options = [get(blob, path, None) for path in action.input_path_options]
-                condition = action.options_condition
-                potential_values = list(
-                    filter(
-                        lambda val: get(val, condition.field) == condition.value,
-                        options,
-                    )
-                )
-
-                if len(potential_values) > 1:
-                    raise RuntimeError(
-                        "Unable to determine input path. Found more than one option satisfying predicate."
-                    )
-
-                if not potential_values:
-                    raise RuntimeError(
-                        "Unable to determine input path. Unable to find option satisfying predicate."
-                    )
-
-                value = potential_values[0]
-                # TODO: Pass off Foreign key to other TOML Object (Address) for handling
-            else:
-                value = get(blob, action.input_path)
-
-            set_(self.mapped_object, action.output_path, value)
-        return self.mapped_object
-
-    def postprocess(self, blob):
-        # TODO
-        return self.mapped_object
